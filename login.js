6336a99159eb368254389b3c6a81616d976134ae
gbraga2001 <110977326+gbraga2001@users.noreply.github.com>
2023-03-24 07:30:14 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,6 @@
+const googleLoginButton = document.querySelector(".google-login button");
+
+googleLoginButton.addEventListener("click", () => {
+  // Implement Google login here
+  console.log("Login with Google clicked");
+});
